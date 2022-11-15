3c2ca13c17653230577f9504180e7385915758a2
Arthur Bequie <arthurbequie@protonmail.com>
2022-11-15 15:58:05 +0100

    No auth for test dev

--- api/store/views.py
+++ api/store/views.py
@@ -1,6 +1,8 @@
 from rest_framework.viewsets import ReadOnlyModelViewSet, ModelViewSet
 from rest_framework.response import Response
 from rest_framework.decorators import action
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
 
 from store.permissions import IsAdminAuthenticated, IsStaffAuthenticated
 from store.models import Category, Product, Article
@@ -12,6 +14,7 @@ class MultipleSerializerMixin:
 
     detail_serializer_class = None
 
+    @method_decorator(csrf_exempt)
     def get_serializer_class(self):
         if self.action == 'retrieve' and self.detail_serializer_class is not None:
             return self.detail_serializer_class
@@ -31,9 +34,11 @@ class CategoryViewset(MultipleSerializerMixin, ReadOnlyModelViewSet):
     serializer_class = CategoryListSerializer
     detail_serializer_class = CategoryDetailSerializer
 
+    @method_decorator(csrf_exempt)
     def get_queryset(self):
         return Category.objects.filter(active=True)
 
+    @method_decorator(csrf_exempt)
     @action(detail=True, methods=['post'])
     def disable(self, request, pk):
         self.get_object().disable()
@@ -45,6 +50,7 @@ class ProductViewset(MultipleSerializerMixin, ReadOnlyModelViewSet):
     serializer_class = ProductSerializer
     detail_serializer_class = ProductDetailSerializer
 
+    @method_decorator(csrf_exempt)
     def get_queryset(self):
         queryset = Product.objects.filter(active=True)
         category_id = self.request.GET.get('category_id')
@@ -52,6 +58,7 @@ class ProductViewset(MultipleSerializerMixin, ReadOnlyModelViewSet):
             queryset = queryset.filter(category_id=category_id)
         return queryset
 
+    @method_decorator(csrf_exempt)
     @action(detail=True, methods=['post'])
     def disable(self, request, pk):
         self.get_object().disable()
@@ -67,6 +74,7 @@ class ArticleViewset(ReadOnlyModelViewSet):
 
     serializer_class = ArticleSerializer
 
+    @method_decorator(csrf_exempt)
     def get_queryset(self):
         queryset = Article.objects.filter(active=True)
         product_id = self.request.GET.get('product_id')
