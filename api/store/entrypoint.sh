f82b368f7e6db63809742129f8ec6e85717a126f
RamyFerhat59 <ramy.ferhat@epitech.eu>
2022-11-15 14:42:59 +0100

    FIX ENTRYPOINT

---
+++ api/store/entrypoint.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+pyhton manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@dev.org', 'adminpass')"
+python manage.py migrate
+python manage.py runserver 0.0.0.0:8000
+
