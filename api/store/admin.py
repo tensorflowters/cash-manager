c0711b9f270950a47308677c5fe470f77a2d0bde
RamyFerhat59 <ramy.ferhat@epitech.eu>
2022-11-21 14:12:22 +0100

    FIX DEPLOY

--- api/store/admin.py
+++ api/store/admin.py
@@ -4,17 +4,17 @@ from store.models import Category, Product, Article
 
 class CategoryAdmin(admin.ModelAdmin):
 
-    list_display = ('name', 'active', 'description', 'date_created', 'date_updated')
+    list_display = ('name', 'active', 'description', 'url' 'date_created', 'date_updated')
 
 
 class ProductAdmin(admin.ModelAdmin):
 
-    list_display = ('name', 'description', 'active', 'category', 'date_created', 'date_updated')
+    list_display = ('name', 'description', 'active', 'category','url', 'date_created', 'date_updated')
 
 
 class ArticleAdmin(admin.ModelAdmin):
 
-    list_display = ('name', 'description', 'price', 'active', 'in_stock_quantity', 'out_stock_quantity', 'date_created', 'date_updated')
+    list_display = ('name', 'description', 'price', 'active', 'url', 'in_stock_quantity', 'out_stock_quantity', 'date_created', 'date_updated')
 
     @admin.display(description='Category')
     def category(self, obj):
