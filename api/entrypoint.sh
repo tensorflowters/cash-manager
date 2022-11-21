584ab26a9eb11d2d643d06edc60e9562edd7e0de
RamyFerhat59 <ramy.ferhat@epitech.eu>
2022-11-21 15:23:46 +0100

    REMOVE ADMIN CREATION

--- api/entrypoint.sh
+++ api/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-python manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@dev.org', 'adminpass')"
+# python manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@dev.org', 'adminpass')"
 python manage.py makemigrations
 python manage.py migrate
 python manage.py runserver 0.0.0.0:8000
