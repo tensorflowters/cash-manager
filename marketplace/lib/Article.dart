42f117333986b2da5351d612cb692373bdf5e976
Matteo ANDRE <mandre@esrifrance.fr>
2022-12-20 10:01:56 +0100

    Fix Article class

--- marketplace/lib/Article.dart
+++ marketplace/lib/Article.dart
@@ -7,21 +7,42 @@ class Article {
   late String _articleName;
   late String _url;
   late String _description;
-  late Float _price;
+  late double _price;
+  late int _quantity;
 
-  Article(int articleID, String articleName, String url, String description, Float price) {
+  Article(int articleID, String articleName, String url, String description,
+      double price, int quantity) {
     _articleID = articleID;
     _articleName = articleName;
     _url = url;
     _description = description;
     _price = price;
-  },
+    _quantity = quantity;
+  }
   String getArticleName() {
     return _articleName;
   }
 
-  String getUrl(){
+  String getUrl() {
     return _url;
+  }
+
+  double getPrice() {
+    return _price;
+  }
+
+  int getQuantity() {
+    return _quantity;
+  }
+
+  int setQuantity(int qte) {
+    _quantity = _quantity + qte;
+    if (_quantity < 1) {
+      _quantity = 0;
+      return _quantity;
+    } else {
+      return _quantity;
+    }
   }
 
   // Widget getArticleWidget() {
